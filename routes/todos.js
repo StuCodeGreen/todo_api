4e63afae774100c602f131ae06bc6e49af351790
StuCodeGreen <m.kuznecovas@gmail.com>
2018-08-12 12:02:14 +0100

    defined all 5 routes,crud

--- routes/todos.js
+++ routes/todos.js
@@ -33,4 +33,24 @@ router.get('/:todoId', function(req, res) {
     });
 });
 
+router.put('/:todoId', function(req, res) {
+  db.Todo.findOneAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
+    .then(function(todo) {
+      res.json(todo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
+router.delete('/:todoId', function(req, res) {
+  db.Todo.remove({ _id: req.params.todoId })
+    .then(function() {
+      res.json({ message: 'deleted' });
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
 module.exports = router;
